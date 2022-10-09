41c0e9a8624a005e2afe83fb6a20ab06378aae26
Réda Housni Alaoui <reda-alaoui@hey.com>
2022-10-09 20:55:12 +0200

    Init

---
+++ src/main/java/me/redaalaoui/ro_rw_routing/RoRwRoutingApplication.java
@@ -0,0 +1,13 @@
+package me.redaalaoui.ro_rw_routing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoRwRoutingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoRwRoutingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/redaalaoui/ro_rw_routing/RoRwRoutingApplicationTests.java
@@ -0,0 +1,13 @@
+package me.redaalaoui.ro_rw_routing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoRwRoutingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
